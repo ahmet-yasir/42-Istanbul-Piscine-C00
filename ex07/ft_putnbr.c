a69d8056e543823d6765ddf181f1ad46a71bdcd0
Ahmet Yasir Kulaksiz <akulaksi@k1m15s10.42istanbul.com.tr>
2024-02-01 15:19:11 +0300

    ex07 0 düzeltildi.

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,12 +6,11 @@
 /*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 13:39:52 by akulaksi          #+#    #+#             */
-/*   Updated: 2024/02/01 14:59:14 by akulaksi         ###   ########.fr       */
+/*   Updated: 2024/02/01 15:18:29 by akulaksi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-#include <stdio.h>
 
 void	ft_putnbr_print(int nb)
 {
@@ -27,18 +26,13 @@ void	ft_putnbr_print(int nb)
 	}
 }
 
-void	ft_putnbr(int nb)
+void	ft_putnbr_reverse(int nb)
 {
 	int	temp;
 	int	place_value;
 	int	new_num;
 
 	temp = nb;
-	if (temp < 0)
-	{
-		temp = (-1) * temp;
-		write(1, "-", 1);	
-	}
 	place_value = 1;
 	new_num = 0;
 	while (temp > 0)
@@ -47,14 +41,23 @@ void	ft_putnbr(int nb)
 		place_value *= 10;
 	}
 	place_value /= 10;
-	temp = nb;
-	if (temp < 0)
-		temp = (-1) * temp;
-	while (temp > 0)
+	while (nb > 0)
 	{
-		new_num += place_value * (temp % 10);
-		temp /= 10;
+		new_num += place_value * (nb % 10);
+		nb /= 10;
 		place_value /= 10;
 	}
 	ft_putnbr_print(new_num);
+}
+
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb *= (-1);
+	}
+	else if (nb == 0)
+		write(1, "0", 1);
+	ft_putnbr_reverse(nb);
 }
