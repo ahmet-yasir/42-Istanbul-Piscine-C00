b8d29112bbdf4daf0daef7cc965f77f82fc95221
Ahmet Yasir Kulaksiz <akulaksi@k1m17s02.42istanbul.com.tr>
2024-02-06 15:09:41 +0300

    norminette update

--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,61 +6,65 @@
 /*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 13:39:52 by akulaksi          #+#    #+#             */
-/*   Updated: 2024/02/05 06:31:20 by akulaksi         ###   ########.fr       */
+/*   Updated: 2024/02/06 15:09:08 by akulaksi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 #include <stdlib.h>
 
-void ft_print(char *str)
+void	ft_print(int *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        write(1, &str[i], 1);
-        i++;
-    }
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
 }
 
-char	*ft_putnbr_to_string(int nb)
+int	*ft_putnbr_to_string(int nb)
 {
-	char *str;
-    int temp = nb;
-    int size = 0;
+	int	*result_str;
+	int	i;
+	int	temp;
+	int	size;
 
-    while (temp > 0)
-    {
-        temp /= 10;
-        size++;
-    }
-
-	str = (char *)malloc((size + 1) * sizeof(char));
-    temp = nb;
-    int i = size - 1;
-    while (temp > 0)
-    {
-        str[i] = (temp % 10) + '0';
-        temp /= 10;
-        i--;
-    }
-    str[size] = '\0';
-    return str;
+	temp = nb;
+	size = 0;
+	while (temp > 0)
+	{
+		temp /= 10;
+		size++;
+	}
+	result_str = (int *)malloc((size + 1) * sizeof(int));
+	temp = nb;
+	i = size - 1;
+	while (temp > 0)
+	{
+		result_str[i] = (temp % 10) + '0';
+		temp /= 10;
+		i--;
+	}
+	result_str[size] = '\0';
+	return (result_str);
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
+	int	*str;
+
 	if (nb == -2147483648)
-		write(1, "-2147483648",12);
+		write(1, "-2147483648", 12);
 	else if (nb == 0)
 		write(1, "0", 1);
 	else if (nb < 0)
-    {
-        write(1, "-", 1);
-        nb *= (-1);
-    }
-    char *str = ft_putnbr_to_string(nb);
-    ft_print(str);    
+	{
+		write(1, "-", 1);
+		nb *= (-1);
+	}
+	str = ft_putnbr_to_string(nb);
+	ft_print(str);
 }
