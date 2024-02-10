73bb7f23d42a2b3930d46e00ebd4fbcca4ae3da0
Ahmet Yasir Kulaksiz <akulaksi@k1m23s08.42istanbul.com.tr>
2024-02-10 12:00:28 +0300

    norminette

--- ex06/ft_print_comb2.c
+++ ex06/ft_print_comb2.c
@@ -6,36 +6,40 @@
 /*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 13:35:38 by akulaksi          #+#    #+#             */
-/*   Updated: 2024/02/07 08:55:21 by akulaksi         ###   ########.fr       */
+/*   Updated: 2024/02/10 11:58:32 by akulaksi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-    write(1, &c, 1);
+	write(1, &c, 1);
 }
 
-void ft_print_comb2(void) {
-    int i = 0;
-    while (i <= 98) {
-        int j = i + 1;
-        while (j <= 99) {
-            ft_putchar((i / 10) + '0');
-            ft_putchar((i % 10) + '0');
-            ft_putchar(' ');
-            ft_putchar((j / 10) + '0');
-            ft_putchar((j % 10) + '0');
+void	ft_print_comb2(void)
+{
+	int	i;
+	int	j;
 
-            if (i != 98 || j != 99) {
-                ft_putchar(',');
-                ft_putchar(' ');
-            }
-
-            j++;
-        }
-        i++;
-    }
-    ft_putchar('\n');
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			ft_putchar((i / 10) + '0');
+			ft_putchar((i % 10) + '0');
+			ft_putchar(' ');
+			ft_putchar((j / 10) + '0');
+			ft_putchar((j % 10) + '0');
+			if (i != 98 || j != 99)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	ft_putchar('\n');
 }
--- ex07/ft_putnbr.c
+++ ex07/ft_putnbr.c
@@ -6,44 +6,46 @@
 /*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 13:39:52 by akulaksi          #+#    #+#             */
-/*   Updated: 2024/02/07 09:07:10 by akulaksi         ###   ########.fr       */
+/*   Updated: 2024/02/10 11:54:12 by akulaksi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
-void ft_print_number(int nb)
+void	ft_print_number(int nb)
 {
-    int temp;
-    int digitCount;
+	int	temp;
+	int	digit_count;
+	int	digit_char;
 
 	temp = nb;
-	digitCount = 1;
-    while (temp >= 10) {
-        temp /= 10;
-        digitCount *= 10;
-    }
-    while (digitCount > 0)
+	digit_count = 1;
+	while (temp >= 10)
 	{
-        char digitChar = '0' + nb / digitCount;
-        write(1, &digitChar, 1);
-        nb %= digitCount;
-        digitCount /= 10;
-    }
+		temp /= 10;
+		digit_count *= 10;
+	}
+	while (digit_count > 0)
+	{
+		digit_char = '0' + nb / digit_count;
+		write(1, &digit_char, 1);
+		nb %= digit_count;
+		digit_count /= 10;
+	}
 }
 
-void ft_putnbr(int nb)
+void	ft_putnbr(int nb)
 {
-    if (nb < 0)
+	if (nb < 0)
 	{
-        write(1, "-", 1);
-        if (nb == -2147483648) 
+		write(1, "-", 1);
+		if (nb == -2147483648)
 		{
-            write(1, "2", 1);
-            nb = 147483648;
-        }
+			write(1, "2", 1);
+			nb = 147483648;
+		}
 		else
-            nb = -nb;
-    }
-    ft_print_number(nb);
+			nb = -nb;
+	}
+	ft_print_number(nb);
 }
